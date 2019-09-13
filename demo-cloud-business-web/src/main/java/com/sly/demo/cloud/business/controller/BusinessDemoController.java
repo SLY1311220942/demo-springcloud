1e68ccd5a9c8874a53f97d9fd769370c3aac8ae5
sly <1311220942@qq.com>
2019-09-13 15:38:42 +0800

    提交

--- demo-cloud-business-web/src/main/java/com/sly/demo/cloud/business/controller/BusinessDemoController.java
+++ demo-cloud-business-web/src/main/java/com/sly/demo/cloud/business/controller/BusinessDemoController.java
@@ -2,9 +2,15 @@ package com.sly.demo.cloud.business.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/business")
 public class BusinessDemoController {
-
+	
+	@ResponseBody
+	@RequestMapping("/test01")
+	public Object test01() {
+		return "test01";
+	}
 }
